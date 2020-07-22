cf43c042feb6b5c929a47f9c3500bd6da6675237
irrako <irraquex@gmail.com>
2020-07-22 14:16:00 -0400

    corriendo

--- database/connect.js
+++ database/connect.js
@@ -1,3 +1,10 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://172.28.0.2:27017/crud");
+mongoose.connect("mongodb://172.28.0.2:27017/crud", {useNewUrlParser: true});
+var db = mogoose.connection;
+db.on("error", () => {
+    console.log("ERROR no se puede conectar al servidor");
+});
+db.on("open", () => {
+    console.log("conexion exitosa");
+});
 module.exports = mongoose;
